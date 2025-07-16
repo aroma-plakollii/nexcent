b1d39a4a82027134e57246c8b33398644cb845f7
Aroma Plakolli <142112664+aroma-plakollii@users.noreply.github.com>
2025-07-16 14:47:21 +0000

    ndryshime

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,25 @@
 import Image from "next/image"
+import Link from "next/link"
 
 export const NavBar = () => {
     return (
         <header>
             <nav>
                 <div>
-                    <Image src={'/img/Logo.png'} alt="Nexcent Logo" width={155} height={24} />
+                    <div>
+                        <Image src={'/img/Icon.svg'} alt="Nexcent Logo" width={24} height={16} />
+                        <Image src={'/img/Nexcent.svg'} alt="Nexcent Logo" width={77} height={14} />
+                    </div>
+                    <div>
+                        <div>
+                            <Link href={'#'}>Home</Link>
+                            <Link href={'#'}>Features</Link>
+                            <Link href={'#'}>Community</Link>
+                            <Link href={'#'}>Blog</Link>
+                            <Link href={'#'}>Pricing</Link>
+                        </div>
+                        <button>Register Now RightArrowVector.svg</button>
+                    </div>
                 </div>
             </nav>
         </header>
